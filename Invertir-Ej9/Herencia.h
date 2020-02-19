b0814c0ca19bd80b23a76861f10d0cd98f3c7351
Srlu28 <57677447+Srlu28@users.noreply.github.com>
2020-02-19 20:17:22 +0100

    Update Herencia.h

--- Invertir-Ej9/Herencia.h
+++ Invertir-Ej9/Herencia.h
@@ -1,14 +1,14 @@
-#ifndef Herencia_h
+def Herencia_h
 #define Herencia_h
-#include "queue_eda_h.h"
-#include<iostream>
-using namespace std;
+
+#include "Queue.h"
 template <class T>
 class ListaInvierte :public queue<T> {
-	using Nodo = typename queue<T> ::Nodo;
+	using Nodo = typename queue<T>::Nodo;
 public:
-	void print(std::ostream& o = std::cout)const {
-		Nodo *aux = this->prim;
+	void print(std::ostream& o = std::cout) const {
+
+		Nodo* aux = this->prim;
 		while (aux != nullptr)
 		{
 			o << aux->elem << " ";
@@ -16,16 +16,27 @@ public:
 		}
 		o << endl;
 	}
-	void invierte(ListaInvierte & lista)
+	void invertir()
 	{
-		if (this->size() >= 1)
+		Nodo *tmp = this->prim, *t2 = tmp;
+		Nodo* current = this->prim->sig;
+		this->prim->sig = nullptr;
+		while (current != nullptr) {
+			t2 = current->sig;
+			current->sig = tmp;
+			tmp = current;
+			current = t2;
+		}
+	}
+	void invierte()
+	{
+		if (!this->empty())
 		{
-			T elem = this->front();
-			this->pop();
-			invierte(lista);
-			lista.push(elem);
+			invertir();
+			Nodo* tmp = this->ult;
+			this->ult = this->prim;
+			this->prim = tmp;
 		}
 	}
 };
-
 #endif
