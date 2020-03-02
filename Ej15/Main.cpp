38f5c74315377eb3d4204ffa2f9beb936171d75d
Srlu28 <57677447+Srlu28@users.noreply.github.com>
2020-03-02 18:15:02 +0100

    EJ15 con multiset solo MAIN

--- Ej15/Main.cpp
+++ Ej15/Main.cpp
@@ -1,35 +1,44 @@
 #include <iostream>
 using namespace std;
-
-#include "stack_eda.h"
 #include "queue_eda.h"
-#include "Herencia.h"
-template <class T>
-using Nodo = typename queue<T>::Nodo;
+#include <set>
+
+//He optado en este ejercicio utilizar el multiset con el parametro greater, asi me será facil tener ordenados los elementos mayores
 
 bool resuelveCaso()
 {
 	int numSobres, picks;
 	cin >> numSobres >> picks;
-	if (numSobres == 0 && picks == 0)
-	{
-		return false;
-	}
+	if (numSobres == 0 && picks == 0)return false;
 	else
 	{
-		Herencia<int> sobres;
-		for (int i = 0; i < numSobres; i++)
+		multiset<int, greater<int>> conjunto;
+		queue<int> lista;
+		int elem;
+		for (int pos = 0; pos < picks; pos++)
 		{
-			int n;
-			cin >> n;
-			sobres.push(n);
+			cin >> elem;
+			lista.push(elem);
+			conjunto.insert(elem);
 		}
-		int posibilidades=numSobres-picks+1;
-		stack<int>res = sobres.ej(posibilidades,picks);
-		res.mostrar();
+		int res = *conjunto.begin();
+		cout << res << " ";
+		for (int i = 0; i < numSobres - picks; i++)
+		{
+			cin >> elem;
+			auto it = conjunto.find(lista.front());
+			conjunto.erase(it);
+			lista.pop();
+			conjunto.insert(elem);
+			lista.push(elem);
+			int res = *conjunto.begin();
+			cout << res << " ";
+		}
+		cout << endl;
 		return true;
 	}
 }
+
 int main()
 {
 	while (resuelveCaso());
